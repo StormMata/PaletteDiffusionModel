595d1d47fb6cd5d30a17cd2f280a7b15422a6591
StormMata <storm.mata097@gmail.com>
2024-09-14 21:56:01 -0600

    new case

--- libraries/data/dataset.py
+++ libraries/data/dataset.py
@@ -242,6 +242,31 @@ def tensor_transforms(tensors, data_bounds):
         print("u:", x[0].min(), x[0].max())
         print("v:", x[1].min(), x[1].max())
 
+    elif x.shape[0] == 3:
+        assert x.shape[0] == 3, f"tensor_transforms assumes 3 channel data! Shape of x is {x.shape}"
+
+        # Rescale to [-1,1]
+        umin, umax, vmin, vmax, hpdmin, hpdmax, dpymin, dpymax = data_bounds
+
+        print("STORM DEBUGGING ON 3-CHANNEL DATA")
+        print("min/max before scaling")
+        print("u:",    x[0].min(), x[0].max())
+        print("v:",    x[1].min(), x[1].max())
+
+        # Check that data is within expected bounds
+        assert (x[0,:,:].min() >= umin)    & (x[0,:,:].max() <= umax),    f"Unexpected u values! min/max    = {x[0,:,:].min()}/{x[0,:,:].max()}"
+        assert (x[1,:,:].min() >= vmin)    & (x[1,:,:].max() <= vmax),    f"Unexpected v values! min/max    = {x[1,:,:].min()}/{x[1,:,:].max()}"
+
+        x[0,:,:] = 2*(x[0,:,:] - umin)/(umax - umin) - 1
+        x[1,:,:] = 2*(x[1,:,:] - vmin)/(vmax - vmin) - 1
+
+        y[0,:,:] = 2*(y[0,:,:] - umin)/(umax - umin) - 1
+        y[1,:,:] = 2*(y[1,:,:] - vmin)/(vmax - vmin) - 1
+
+        print("min/max after scaling")
+        print("u:", x[0].min(), x[0].max())
+        print("v:", x[1].min(), x[1].max())
+
     else:
         raise ValueError(f"Expected 1, 2, 4, 5, or 6 channel data, but got {x.shape[0]} channels")
 
