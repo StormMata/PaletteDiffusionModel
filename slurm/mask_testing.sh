192782466a7bf4f638f18e74c1704b581561af6e
StormMata <storm.mata097@gmail.com>
2024-09-22 19:39:52 -0400

    new mask test

---
+++ slurm/mask_testing.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J mask_test                # job name
+#SBATCH --nodes=1                  #
+#SBATCH --time=0-01:00:00          # set time limit
+#SBATCH --partition=gpu-h100      # GPU partition
+#SBATCH --account=awaken           # allocation
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:h100:4          # GPU request
+#SBATCH --output=logs/%x.%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=storm@mit.edu
+#SBATCH --exclusive                # reserve entire node
+#SBATCH --qos=high
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export MPICH_GPU_SUPPORT_ENABLED=1
+OMPI_MCA_opal_cuda_support=true
+
+conda run --no-capture-output -n diffusion python3 run.py -c config/mask_testing.json -gpu 0,1,2,3 -b 32
