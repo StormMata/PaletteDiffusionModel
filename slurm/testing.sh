c789f1ea850296b927ad80e80da580bcdbca4baa
StormMata <storm.mata097@gmail.com>
2024-09-21 20:48:43 -0600

    new case

--- slurm/testing.sh
+++ slurm/testing.sh
@@ -2,7 +2,7 @@
 #SBATCH -J test                # job name
 #SBATCH --nodes=1                  #
 #SBATCH --time=0-01:00:00          # set time limit
-#SBATCH --partition=gpu-h100      # GPU partition
+#SBATCH --partition=gpu-h100s      # GPU partition
 #SBATCH --account=awaken           # allocation
 #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=1
